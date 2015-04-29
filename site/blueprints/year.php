5c24556173ed26f90af041c9cae0e005766d3c83
jordanhoagbin <jordan.hoagbin@me.com>
2015-04-29 11:04:44 -0700

    Year Blueprint
    Cool

---
+++ site/blueprints/year.php
@@ -0,0 +1,30 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Year
+pages:
+  build:
+    - title: Schedule
+      uid: schedule
+      template: day
+
+    - title: Projects
+      uid: projects
+      template: projects
+
+    - title: Presenters
+      uid: presenters
+      template: presenters
+
+    - title: Area Guide
+      uid: area-guide
+      template: area-guide
+
+
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+  text:
+    label: Text
+    type:  textarea
