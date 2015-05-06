b7465ee6619ad4f217535e4aa6039d660c8cd058
August Miller <hello@gusmiller.com>
2015-05-05 22:32:01 -0700

    Projects don't need presenters.
    We can connect it through events, but there aren’t implicitly
    presenters for a given project, until we connect them with an event.

--- site/blueprints/project.php
+++ site/blueprints/project.php
@@ -8,14 +8,6 @@ fields:
   title: 
     label: Project Name
     type: text
-  presenter:
-    label: Presenter
-    type: select
-    options: query
-    query: 
-      page: ../../presenters 
-      fetch: children
-      text: {{title}}
   description:
     label: Description
     type: textarea
