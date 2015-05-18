53ebd88494fb7980f61ef73b72640c243963f786
jordanhoagbin <jordan.hoagbin@me.com>
2015-05-18 10:12:31 -0700

    Year Homepage Content PHP

--- site/templates/year.php
+++ site/templates/year.php
@@ -4,7 +4,14 @@
   <section class="intro">
     <article>
       <div class="column full">
-        <h2><?= $page->title()->html() ?></h2>
+
+        <h2><?= $page->date_text()->kirbytext() ?></h2>
+
+        <h1><?= $page->introduction()->kirbytext() ?></h1>
+
+        <h2><?= $page->location()->kirbytext() ?></h2>
+
+        <p><?= $page->description()->kirbytext() ?></p>
       </div>
     </article>
   </section>
