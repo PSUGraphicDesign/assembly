5233234ca20c37cae9f5163783769ceed7deddf1
August Miller <hello@gusmiller.com>
2015-05-19 22:12:04 -0700

    Introduce contextually-aware footer content

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,2 +1,20 @@
-</body>
+    <footer>
+      <section>
+        <article>
+          <div class="column quarter">
+            <h4>Many Thanks</h4>
+            <? snippet('thanks', ['year' => AB::contextual_year()]) ?>
+          </div>
+          <div class="column half sponsors">
+            <h4>Sponsors</h4>
+            <? snippet('sponsors', ['year' => AB::contextual_year()]) ?>
+          </div>
+          <div class="column quarter social">
+            <h4>Find Us</h4>
+            <?= $site->social_links()->kirbytext() ?>
+          </div>
+        </article>
+      </section>
+    </footer>
+  </body>
 </html>
