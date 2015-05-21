ed6ea85feaee1e3a770bf36fc764a01ba4bbbc28
August Miller <hello@gusmiller.com>
2015-05-20 22:58:52 -0700

    Pull maps integration into custom schedule script

--- assets/js/assembly.js
+++ assets/js/assembly.js
@@ -1,43 +1,34 @@
-$(function () {
-  if ( $('.map').length ) {
-    map_init('#map-canvas');
-  }
-});
+window.Assembly = window.Assembly || {
+  Classes: {},
+  UI: {},
+  Storage: {}
+};
 
-function map_init (map) {
-  var lat = $(map).data('center-lat'),
-      lng = $(map).data('center-lng'),
-      zoom = $(map).data('zoom')
-  var mapOptions = {
-    zoom: zoom,
-    center: new google.maps.LatLng(lat, lng),
-    disableDefaultUI: true,
-    styles: [
-      {
-        "stylers": [
-          { "hue": "#003bff" },
-          { "saturation": 100 }
-        ]
-      },{
-        "featureType": "landscape.natural",
-        "stylers": [
-          { "visibility": "off" }
-        ]
-      },{
-        "featureType": "road.highway",
-        "stylers": [
-          { "visibility": "simplified" }
-        ]
-      }
+window.Assembly.Storage.map_styles = [
+  {
+    "stylers": [
+      { "hue": "#003bff" },
+      { "saturation": 100 }
     ]
-  };
+  },{
+    "featureType": "landscape.natural",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "road.highway",
+    "stylers": [
+      { "visibility": "simplified" }
+    ]
+  }
+];
 
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-  var marker = new google.maps.Marker({
-      position: mapOptions.center,
-      map: map,
-      title: 'Hello World!'
+$(function () {
+  if ( $('.map').length ) new window.Assembly.Classes.Schedule({
+    map_options: window.Assembly.Storage.map_options,
+    map: "#map",
+    day_selector: ".day",
+    event_selector: ".event",
+    map_settings: {}
   });
-}
+});
---
+++ assets/js/schedule.js
@@ -0,0 +1,97 @@
+/*
+  Assembly Schedule & Map Manager
+  August W. Miller, Jordan Hoagbin
+  2015
+*/
+
+// Schedule Organizer
+
+window.Assembly.Classes.Schedule = window.Assembly.Classes.Schedule || function (opts) {
+  this.options = $.extend(true, {
+    map_container: $('.map-container'),
+    map_settings: {
+      zoom: 13,
+      center: new google.maps.LatLng($('.map-container').data('center-lat'), $('.map-container').data('center-lng'))
+    }
+  }, opts);
+
+  this.days = [];
+
+  this.init();
+
+  console.log(this);
+}
+
+window.Assembly.Classes.Schedule.prototype = {
+  init: function () {
+    this.map = new google.maps.Map(this.options.map_container.get(0), this.options.map_settings);
+
+    $(this.options.day_selector).each((function (_this) {
+      return function (index, el) {
+        _this.days.push(new window.Assembly.Classes.Day(el, _this));
+      }
+    })(this));
+  },
+}
+
+
+// Individual Days
+
+window.Assembly.Classes.Day = window.Assembly.Classes.Day || function (el, schedule) {
+  this.schedule = schedule;
+  this.el = $(el);
+  this.event_container = this.el.find('.events');
+
+  this.events = [];
+
+  this.init();
+}
+
+window.Assembly.Classes.Day.prototype = {
+  init: function () {
+    $.each(this.event_container.find('.event'), (function (_this) {
+      return function (index, el) {
+        _this.events.push(new window.Assembly.Classes.Event(el, _this));
+      }
+    })(this));
+
+    this.listen();
+    this.blur();
+  },
+  listen: function () {
+    this.el.find('h3').on('click', (function (_this) {
+      return function (e) {
+        _this.focus();
+      }
+    })(this));
+  },
+  focus: function () {
+    this.event_container.show();
+  },
+  blur: function () {
+    this.event_container.hide();
+  }
+}
+
+
+// Events
+
+window.Assembly.Classes.Event = window.Assembly.Classes.Event || function (el, day) {
+  this.el = $(el);
+  this.day = day;
+  this.schedule = day.schedule;
+  this.lat = this.el.data('lat');
+  this.lng = this.el.data('lng');
+
+  this.init();
+}
+
+window.Assembly.Classes.Event.prototype = {
+  init: function () {
+    this.pin = new google.maps.Marker({
+      position: new google.maps.LatLng(this.lat, this.lng),
+      map: this.schedule.map,
+      title: this.el.find('.event-title').text()
+    });
+  }
+}
