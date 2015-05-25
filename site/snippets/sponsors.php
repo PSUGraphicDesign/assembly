c7b315b3debd3bf471c235130ba2d8d479de9998
August W. Miller <hello@gusmiller.com>
2015-05-25 10:13:44 -0700

    Tried some new shit in the sponsor footer, didn't work
    It was an opportunity to do a bit of refactoring, cleanup, though. The
    columns mixing (while I don’t like falling back on this CSS3 feature),
    is more flexible, and allows us to work with tablet and mobile more
    easily.

--- site/snippets/sponsors.php
+++ site/snippets/sponsors.php
@@ -1,1 +1,17 @@
-<?= AB::current_year()->sponsor_list()->kirbytext() ?>
+<article class="sponsors">
+  <div class="column full">
+    <h5>A Special Thanks to Our Sponsors</h5>
+  </div>
+  <div class="column full sponsor-list">
+    <?= AB::current_year()->sponsor_list()->kirbytext() ?>
+  </div>
+  <? /* foreach ( AB::sponsors_in_chunks(3) as $chunk ) { ?>
+    <div class="column third">
+      <ul>
+        <? foreach ( $chunk as $sponsor ) { ?>
+          <li><?= kirbytext($sponsor) ?></li>
+        <? } ?>
+      </ul>
+    </div>
+  <? } */ ?>
+</article>
