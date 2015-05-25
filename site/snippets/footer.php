f6e7ae98b5b8c6a98517a73c23caa450ba5aece7
jordanhoagbin <jordan.hoagbin@me.com>
2015-05-24 18:55:55 -0700

    Footer Sponsor Snippet

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,23 +1,26 @@
     <footer>
-      <section>
+
+      <section class="sponsorfoot">
         <article>
 
-         <div class="column quarter">
+      <!-- <div class="column quarter">
             <h5>Many Thanks</h5>
             <?php /* <? snippet('thanks', ['year' => AB::contextual_year()]) ?> */ ?>
-          </div>
+          </div> -->
 
-          <div class="column half sponsors">
+          <div class="column full">
             <h5>Sponsors</h5>
-              <?= $site->sponsor_list()->kirbytext() ?>
+              <?= snippet('sponsors') ?>
           </div>
 
-          <div class="column quarter social">
-            <h5>Find Us</h5>
-            <?= $site->social_links()->kirbytext() ?>
-          </div>  
+          
 
-        
+      </section>
+        </article>
+
+      <section class="thefooter">
+        <article>
+
           <div class="column half">
             <div class="thebottom">2015 Assembly</div>
           </div>
--- site/snippets/sponsors.php
+++ site/snippets/sponsors.php
@@ -1,13 +1,1 @@
-<? $sponsors = $year->sponsors()->yaml() ?>
-<? if ( count($sponsors) ) { ?>
-  <ul>
-    <? foreach ( $sponsors as $sponsor ) { ?>
-      <? $logo = $year->image($sponsor['logo']); ?>
-      <li>
-        <a href="<?= $sponsor['url'] ?>" target="_blank">
-          <img src="<?= $logo->url() ?>" alt="<?= $sponsor['name'] ?>" />
-        </a>
-      </li>
-    <? } ?>
-  </ul>
-<? } ?>
+<?= AB::current_year()->sponsor_list()->kirbytext() ?>
